2d3994783b9e66ce0f0b3f1896091ba372db06b7
tamilarasimailsamy <tamilarasi2002k@gmail.com>
2025-04-07 16:25:40 +0530

    indexing_string

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    //let hello = "Здравствуйте";
+    //let s = &hello[0..4];
+//println!("The first letter in {}", s);
+
+//method of iterating over string
+for c in "Зд".chars() {
+    println!("{c}");
+}
+
+for b in "Зд".bytes(){
+    println!("{b}");
+}
+}
